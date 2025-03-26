a3a30cc5a3e72f939fe8346ff91599569b4d8749
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-26 20:18:55 -0300

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App(){
   return (
     <div>
-      <h1 className="">Gerenciador de Tarefas</h1>
+      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
       <Tasks /> 
       <AddTask />
     </div>
