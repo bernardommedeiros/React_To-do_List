c477388599d9ad3b67a87a6db13534568658686f
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-27 20:12:28 -0300

    instalacao lucid e tentativa da barra de tarefas

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,19 @@
-function Tasks(){
-
+function Tasks(props){
+    // key é unica entre itens
     return (
-        <div>
-            <h1>Tasks</h1>
-        </div>
-    )
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+            {props.tasks.map(task) => (
+                <li key={task.id} className="flex">
+                    {task.title}
+
+                    <button className="bg-slate-400 text-white p-2 rounded-md ">{tasks.title}</button>
+                    <button>Ver detalhes</button>
+                   
+                </li>
+            ))}
+        </ul>
+    );
 }
+
 
 export default Tasks;
