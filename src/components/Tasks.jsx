08c5ccc258f40cdf94c0a98a3d8928e2888fa593
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-28 19:55:16 -0300

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,17 +1,19 @@
+import { ChevronRightIcon } from "lucide-react";
+
 function Tasks(props){
     // key é unica entre itens
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-            {props.tasks.map(task) => (
-                <li key={task.id} className="flex">
-                    {task.title}
-
-                    <button className="bg-slate-400 text-white p-2 rounded-md ">{tasks.title}</button>
-                    <button>Ver detalhes</button>
-                   
-                </li>
-            ))}
+            {props.tasks.map((task) => (
+            <li key={task.id} className="flex gap-2">
+            <button className="bg-slate-400 w-full text-left text-white p-2 rounded-md">{task.title}</button>
+            <button className="bg-slate-400 text-white p-2 rounded-md">
+                <ChevronRightIcon></ChevronRightIcon>
+            </button>
+            </li>
+            ))}         
         </ul>
+        
     );
 }
 
